01de89c540400fe80a79c0c8ceec9fddb0df91cc
RamonSantos10 <ramontedescodossantos2020@gmail.com>
2025-04-16 12:43:07 -0300

    Configuração inicial do processo de cadastro com Firebase Authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,31 +11,16 @@ const Navbar = () => {
               <span>Life</span>Dev
             </li>
           </NavLink>
-          <NavLink  to='/login' className={styles.brand}>
+          <NavLink  to='/login' className={styles.brand} activeClassName={styles.active}>
             <li>
               <span>Login</span>
             </li>
           </NavLink>
-          <NavLink to='/register' className={styles.brand}>
+          <NavLink to='/register' className={styles.brand} activeClassName={styles.active}>
             <li>
               <span>Register</span>
             </li>
           </NavLink>
-          {/* <NavLink to='/NewPost' className={styles.brand} activeClasName={styles.active}>
-            <li>
-              <span>NewPost</span>
-            </li>
-          </NavLink>
-          <NavLink to='/dashboard' className={styles.brand} activeClasName={styles.active}>
-            <li>
-              <span>Dashboard</span>
-            </li>
-          </NavLink>
-          <NavLink to='/AboutUs' className={styles.brand} activeClasName={styles.active}>
-            <li>
-              <span>About Us</span>
-            </li>
-          </NavLink> */}
           <li>
             <button className={styles.exit}>Exit</button>
           </li>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthentication } from '../../hooks/useAuthentication';
 
 const Register = () => {
+
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const {createUser, error: authError, loading} = useAuthentication()
+
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+    
+      setError("")
+
+      const user = {
+        displayName,
+        email,
+        password,
+      }
+
+      password !== confirmPassword && setError('As senham não coincidem')
+
+      const res = await createUser(user)
+
+      console.log(res);
+
+      
+    }
+
   return (
-    <div>Register</div>
+    <div className={""}>
+      <h1>Entrar</h1>
+      <p>Faça login em nossa plataforma de desenvolvedores</p>
+      <form onSubmit={handleSubmit}>
+      <label>
+          <span>Nome: </span>
+          <input
+            type='text'
+            name='displayName'
+            required
+            placeholder='Nome do Usuário'
+            onChange={(e) => setDisplayName(e.target.value)}
+            value={displayName}
+          />
+        </label>
+        <label>
+          <span>E-mail: </span>
+          <input
+            type='email'
+            name='email'
+            required
+            placeholder='E-mail do usuário'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>Senha: </span>
+          <input
+            type='password'
+            name='password'
+            required
+            placeholder='Insira sua senha'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        <label>
+          <span>Confirmar Senha: </span>
+          <input
+            type='password'
+            name='confirmPassword'
+            required
+            placeholder='Insira sua senha'
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            value={confirmPassword}
+          />
+        </label>
+        {!loading && <button className='btn'>Cadastrar</button>}
+        {loading && <button className='btn' disabled>Aguarde... </button>}
+        {error && <p>{error}</p>}
+      </form>
+    </div>
   )
 }
 
