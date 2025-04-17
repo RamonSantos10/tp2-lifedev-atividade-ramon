3f215415dab3d7cb7ea72b6641b9cb9de289bcd2
RamonSantos10 <ramontedescodossantos2020@gmail.com>
2025-04-17 20:09:02 -0300

    Arrumando erro da home

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,14 @@
+import Layout from '../../components/Layout'
+import { auth } from '../../firebase/config'
+import styles from './Landing.module.css'
+
+const Landing = () => {
+  return (
+    <Layout>
+      <h1>Life Dev</h1>
+      <h3>Entre na nossa plataforma e venha ver o dia a dia de um dev!</h3>
+    </Layout>
+  )
+}
+
+export default Landing
