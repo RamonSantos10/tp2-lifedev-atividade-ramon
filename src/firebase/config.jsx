d99e9282227baa2a3cb4fd65385bcd38ef43a330
RamonSantos10 <ramontedescodossantos2020@gmail.com>
2025-04-18 17:34:06 -0300

    ci: update Firebase configuration and CI workflow
    Update the Firebase service account and project ID in the GitHub Actions workflow to match the new project. Additionally, format the Firebase configuration file to improve readability and consistency.

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,21 +1,21 @@
-import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
-import { getFirestore } from "firebase/firestore"
-import { getAuth } from "firebase/auth"
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxVK8vcF54JYtylORQeps_Sym9OWGeqpA",
-    authDomain: "calculadora-agricultor-react.firebaseapp.com",
-    projectId: "calculadora-agricultor-react",
-    storageBucket: "calculadora-agricultor-react.firebasestorage.app",
-    messagingSenderId: "893159723013",
-    appId: "1:893159723013:web:09229e9450af16afc58c6c",
-    measurementId: "G-0Z4Q22VB2R"
+  apiKey: "AIzaSyCxVK8vcF54JYtylORQeps_Sym9OWGeqpA",
+  authDomain: "calculadora-agricultor-react.firebaseapp.com",
+  projectId: "calculadora-agricultor-react",
+  storageBucket: "calculadora-agricultor-react.firebasestorage.app",
+  messagingSenderId: "893159723013",
+  appId: "1:893159723013:web:09229e9450af16afc58c6c",
+  measurementId: "G-0Z4Q22VB2R",
 };
 
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
-const db = getFirestore(app)
-const auth = getAuth(app)
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db, auth }
+export { db, auth };
